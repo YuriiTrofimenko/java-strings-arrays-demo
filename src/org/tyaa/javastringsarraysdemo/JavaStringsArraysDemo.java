97c4e425cf4d1278f2369e8ebd87cb8a3acb6b4c
YuriiTrofimenko <tyaa@ukr.net>
2019-11-03 11:34:13 +0200

    homework task

--- src/org/tyaa/javastringsarraysdemo/JavaStringsArraysDemo.java
+++ src/org/tyaa/javastringsarraysdemo/JavaStringsArraysDemo.java
@@ -5,6 +5,8 @@
  */
 package org.tyaa.javastringsarraysdemo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author student
@@ -18,6 +20,32 @@ public class JavaStringsArraysDemo {
         System.out.println("Hello Java!");
         // Test comment
         // Test comment 2
+        
+        // Alt + Enter for import
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Input a number then press Enter: ");
+        int x = sc.nextInt();
+        System.out.println("Input the second number then press Enter: ");
+        int y = sc.nextInt();
+        System.out.println("Choose an option: ");
+        System.out.println("1 - concatination");
+        System.out.println("2 - plus");
+        int action = sc.nextInt();
+        if (action == 1) {
+            System.out.println(String.valueOf(x) + String.valueOf(y));
+        } else if (action == 2) {
+            System.out.println(x + y);
+        } else {
+            System.out.println("Incorrect input data!");
+        }
+        /* int x = 10;
+        for (int i = 0; i < 10; i++) {
+            // System.out.println(x++);
+            // System.out.println(++x);
+            x = x + 1;
+            System.out.println(x);
+        } */
     }
     
 }
